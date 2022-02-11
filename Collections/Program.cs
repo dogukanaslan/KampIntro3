d18c8b4bcde6b50599addf04da09416d070428bf
Doğukan ASLAN <dogukanaslan_@hotmail.com>
2022-02-11 18:33:32 +0300

    11 Şubat Ders Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Doğukan", "Nurettin", "Enes", "Kerem" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            //UsingSystemCollectionsGeneric e basılacak.
+            //Listlerin arraylerden farkı arraylerde referansın içindeki eleman sayısı neyse onu yazar.Elemanlar normalde veri kaynağından geleceği için sonradan eleman eklemek istediğinde ekleyemezsin.
+            //List sayesinde listeye sonradan fazladan eleman ekleyebilirsin.
+
+            List<string> isimler2 = new List<string> { "Doğukan", "Nurettin", "Enes", "Kerem" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Eklendi. ");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi. ");
+        }
+
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+    }
+}
+//void emir kipidir.Gider işini yapar ve bitirir. Mesela ben 2 tane sayıyı topladım ve çıkan sonucu başka bir şekilde kullanmak istedim örneğin 2 ile çarpmak istedim. Bunu void ile yapamam.
+//Bunun için RETURN kullanılır.
